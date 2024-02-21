c470ccc5e3851175e9e63e2787065256ed42c497
FABIO HIDEKI KURIKI CESARE <fabio.hkcesare@senacsp.edu.br>
2024-02-20 22:18:58 -0300

    Primeira commit

---
+++ SistemaFarmacia/CadastrarProduto.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SistemaFarmacia
+{
+    partial class CadastrarProduto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botaoVoltar = new System.Windows.Forms.Button();
+            this.botaoCadastrar = new System.Windows.Forms.Button();
+            this.campoQuantidade = new System.Windows.Forms.TextBox();
+            this.campoDescricao = new System.Windows.Forms.TextBox();
+            this.campoNome = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // botaoVoltar
+            // 
+            this.botaoVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoVoltar.Location = new System.Drawing.Point(237, 380);
+            this.botaoVoltar.Name = "botaoVoltar";
+            this.botaoVoltar.Size = new System.Drawing.Size(176, 53);
+            this.botaoVoltar.TabIndex = 25;
+            this.botaoVoltar.Text = "Voltar";
+            this.botaoVoltar.UseVisualStyleBackColor = true;
+            // 
+            // botaoCadastrar
+            // 
+            this.botaoCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoCadastrar.Location = new System.Drawing.Point(19, 380);
+            this.botaoCadastrar.Name = "botaoCadastrar";
+            this.botaoCadastrar.Size = new System.Drawing.Size(176, 53);
+            this.botaoCadastrar.TabIndex = 24;
+            this.botaoCadastrar.Text = "Cadastrar";
+            this.botaoCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // campoQuantidade
+            // 
+            this.campoQuantidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.campoQuantidade.Location = new System.Drawing.Point(173, 240);
+            this.campoQuantidade.Name = "campoQuantidade";
+            this.campoQuantidade.Size = new System.Drawing.Size(240, 26);
+            this.campoQuantidade.TabIndex = 23;
+            // 
+            // campoDescricao
+            // 
+            this.campoDescricao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.campoDescricao.Location = new System.Drawing.Point(173, 182);
+            this.campoDescricao.Name = "campoDescricao";
+            this.campoDescricao.Size = new System.Drawing.Size(240, 26);
+            this.campoDescricao.TabIndex = 22;
+            // 
+            // campoNome
+            // 
+            this.campoNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.campoNome.Location = new System.Drawing.Point(80, 124);
+            this.campoNome.Name = "campoNome";
+            this.campoNome.Size = new System.Drawing.Size(333, 26);
+            this.campoNome.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(19, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Quantidade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Descrição";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Nome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 31);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Cadastrar Produto";
+            // 
+            // CadastrarProduto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 450);
+            this.Controls.Add(this.botaoVoltar);
+            this.Controls.Add(this.botaoCadastrar);
+            this.Controls.Add(this.campoQuantidade);
+            this.Controls.Add(this.campoDescricao);
+            this.Controls.Add(this.campoNome);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CadastrarProduto";
+            this.Text = "CadastrarProduto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botaoVoltar;
+        private System.Windows.Forms.Button botaoCadastrar;
+        private System.Windows.Forms.TextBox campoQuantidade;
+        private System.Windows.Forms.TextBox campoDescricao;
+        private System.Windows.Forms.TextBox campoNome;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ SistemaFarmacia/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace SistemaFarmacia
+{
+    partial class Cadastro
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.campoNome = new System.Windows.Forms.TextBox();
+            this.campoRg = new System.Windows.Forms.MaskedTextBox();
+            this.campoTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.campoPlanoSaude = new System.Windows.Forms.TextBox();
+            this.campoFarmaciaPopular = new System.Windows.Forms.TextBox();
+            this.botaoCadastrar = new System.Windows.Forms.Button();
+            this.botaoVoltar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastrar Cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "RG";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefone";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Plano de Saúde";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 308);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Farmácia Popular";
+            // 
+            // campoNome
+            // 
+            this.campoNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.campoNome.Location = new System.Drawing.Point(73, 85);
+            this.campoNome.Name = "campoNome";
+            this.campoNome.Size = new System.Drawing.Size(333, 26);
+            this.campoNome.TabIndex = 6;
+            // 
+            // campoRg
+            // 
+            this.campoRg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.campoRg.Location = new System.Drawing.Point(73, 145);
+            this.campoRg.Mask = "99.999.999-9";
+            this.campoRg.Name = "campoRg";
+            this.campoRg.Size = new System.Drawing.Size(330, 26);
+            this.campoRg.TabIndex = 7;
+            // 
+            // campoTelefone
+            // 
+            this.campoTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.campoTelefone.Location = new System.Drawing.Point(97, 198);
+            this.campoTelefone.Mask = "(99) 99999-9999";
+            this.campoTelefone.Name = "campoTelefone";
+            this.campoTelefone.Size = new System.Drawing.Size(309, 26);
+            this.campoTelefone.TabIndex = 8;
+            // 
+            // campoPlanoSaude
+            // 
+            this.campoPlanoSaude.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.campoPlanoSaude.Location = new System.Drawing.Point(166, 250);
+            this.campoPlanoSaude.Name = "campoPlanoSaude";
+            this.campoPlanoSaude.Size = new System.Drawing.Size(240, 26);
+            this.campoPlanoSaude.TabIndex = 9;
+            // 
+            // campoFarmaciaPopular
+            // 
+            this.campoFarmaciaPopular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.campoFarmaciaPopular.Location = new System.Drawing.Point(166, 307);
+            this.campoFarmaciaPopular.Name = "campoFarmaciaPopular";
+            this.campoFarmaciaPopular.Size = new System.Drawing.Size(240, 26);
+            this.campoFarmaciaPopular.TabIndex = 10;
+            // 
+            // botaoCadastrar
+            // 
+            this.botaoCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoCadastrar.Location = new System.Drawing.Point(12, 370);
+            this.botaoCadastrar.Name = "botaoCadastrar";
+            this.botaoCadastrar.Size = new System.Drawing.Size(176, 53);
+            this.botaoCadastrar.TabIndex = 11;
+            this.botaoCadastrar.Text = "Cadastrar";
+            this.botaoCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // botaoVoltar
+            // 
+            this.botaoVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoVoltar.Location = new System.Drawing.Point(230, 370);
+            this.botaoVoltar.Name = "botaoVoltar";
+            this.botaoVoltar.Size = new System.Drawing.Size(176, 53);
+            this.botaoVoltar.TabIndex = 12;
+            this.botaoVoltar.Text = "Voltar";
+            this.botaoVoltar.UseVisualStyleBackColor = true;
+            // 
+            // Cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 450);
+            this.Controls.Add(this.botaoVoltar);
+            this.Controls.Add(this.botaoCadastrar);
+            this.Controls.Add(this.campoFarmaciaPopular);
+            this.Controls.Add(this.campoPlanoSaude);
+            this.Controls.Add(this.campoTelefone);
+            this.Controls.Add(this.campoRg);
+            this.Controls.Add(this.campoNome);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Cadastro";
+            this.Text = "Cadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox campoNome;
+        private System.Windows.Forms.MaskedTextBox campoRg;
+        private System.Windows.Forms.MaskedTextBox campoTelefone;
+        private System.Windows.Forms.TextBox campoPlanoSaude;
+        private System.Windows.Forms.TextBox campoFarmaciaPopular;
+        private System.Windows.Forms.Button botaoCadastrar;
+        private System.Windows.Forms.Button botaoVoltar;
+    }
+}
+
